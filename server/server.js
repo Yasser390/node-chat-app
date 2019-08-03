3ca721f7164c3427a9cb7f1a6b0087ab95a3d90f
Yasser390 <yasser.zahran.390@gmail.com>
2019-08-04 00:38:37 +0200

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -19,18 +19,31 @@ io.on('connection',(socket)=>{
     //     text: 'Hey Whatsapp',
     //     createdAt: 123
     // });
-    // socket.emit('newMessage',{          //emits event to a single connection
-    //     from:'fakhry',
-    //     text:'Mantej ya paolo',
-    //     createdAt: 123
-    // });
+    
+    socket.emit('newMessage',{          //emits event to a single connection
+        from:'Admin',
+        text:'Welcome User',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage',{          //emits event to a single connection
+        from:'Admin',
+        text:'A new user joined ya boooo7a',
+        createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage',(message)=>{ 
         console.log('createdMessage',message);
-        io.emit('newMessage',{          //emits event to every connection
+        // io.emit('newMessage',{          //emits event to every connection
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
+        socket.broadcast.emit('newMessage',{           //send it to every connection except the sender
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });           
+        });
     });
     socket.on('disconnect',(socket)=>{
         console.log('User was disconnected');
