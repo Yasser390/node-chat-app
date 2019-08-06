63369c547e47754a9ee5a8a7c038a43333d6474e
Yasser390 <yasser.zahran.390@gmail.com>
2019-08-07 00:42:33 +0200

    scroll to bottom is user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 let socket = io();
 
+function scrollToBottom(){
+    //selectors
+    let messages = jQuery('#messages');
+    let newMessage = messages.children('li:last-child');
+    //heights
+    let clientHeight = messages.prop('clientHeight');
+    let scrollTop = messages.prop('scrollTop');
+    let scrollHeight = messages.prop('scrollHeight');
+    let newMessageHeight = newMessage.innerHeight();
+    let lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop +newMessageHeight +lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    };
+};
 socket.on('connect', function (){
     console.log('Connected to server');
     
@@ -28,6 +43,7 @@ socket.on('newMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 });
 
 socket.on('newLocationMessage',function(message){
@@ -49,6 +65,7 @@ socket.on('newLocationMessage',function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 });
 
@@ -79,5 +96,5 @@ locationButton.on('click',function(){
     },function(){ //is fired if someone tried to fetch location but clicks deny
         locationButton.removeAttr('disabled').text('Send location');           
         alert('Unable to fetch location.');
-    })
+    });
 });
